c78bd1117f236bdebe74c895eb3f480711b9a87f
DG-mfb <dgmfb73@gmail.com>
2020-07-31 12:26:09 +0300

    ading files

--- Convesys.Kernel.Communication.Common/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Convesys.Kernel.Communication.Common
-{
-    public class Class1
-    {
-    }
-}
---
+++ Convesys.Kernel.Communication.Common/IDefinitionProvider`1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Convesys.Kernel.Communication.Common
+{
+    public class IDefinitionProvider<TDefinition> where TDefinition : BaseDefinition
+    {
+    }
+}
