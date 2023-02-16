0159a397cd938936ef26c0d52d77b24b7a2321f4
daniel-georgiev-dgmfb <daniel.georgiev73@gmail.com>
2023-02-16 15:56:21 +0200

    adding spacial project

---
+++ Convesys.Kernel.Spatial/ILocationService.cs
@@ -0,0 +1,9 @@
+﻿namespace Convesys.Kernel.Spatial
+{
+    public interface ILocationService
+    {
+        Task<Tuple<double, double>> GetLocation(Tuple<long, long, double> readings1, Tuple<long, long, double> readings2, Tuple<long, long, double> readings3);
+
+        Task<float> CalculateDistance(Tuple<float, float> coordinatesFrom, Tuple<float, float> coordinatesTo);
+    }
+}
